8d374058e7ed90ff1ce98cf71e7067960aa99e39
Jennvillacis <jennvillacis@gmail.com>
2019-07-25 03:05:52 -0700

    API
    Flickr API

---
+++ application/config/custom_config.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//custom_config.php - a place to save global settings
+
+$config['style'] = 'amelia.css';
+$config['banner'] = 'Default Banner';
+$config['title'] = 'Default Title';
+$config['copyright'] = 'Default Copyright';
+$config['masthead'] = 'Default MastHead';
+$config['theme'] = 'themes/bootswatch/';
+$config['FlickrKey'] = '1e830dcdb2119be1fe75b3317ec10ea3';
+
+$config['nav1'] = array(
+'news' => 'News', 
+'news/create' => 'Add News',
+'pics' => 'Pics',
+);
